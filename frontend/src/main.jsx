1cf0f1dc448e2b5d79831cccae2cb6754a0f54d5
Daffa Ilhami <mdaffailhami@gmail.com>
2024-12-17 21:46:58 +0700

    Test frontend routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import NotFoundPage from "./routes/NotFoundPage.jsx";
+import AboutUsPage from "./routes/about-us/AboutUsPage.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<App />} />
+                <Route path="about-us" element={<AboutUsPage />} />
+                <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>
+);
---
+++ frontend/src/routes/NotFoundPage.jsx
@@ -0,0 +1,14 @@
+export default function NotFoundPage() {
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100vh",
+            }}
+        >
+            <h1>Not Found</h1>
+        </div>
+    );
+}
