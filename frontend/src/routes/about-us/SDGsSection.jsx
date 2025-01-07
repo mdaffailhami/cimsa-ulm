499cb2c226f3a2df9e3faa56dc93b5e1adde82f8
Daffa Ilhami <mdaffailhami@gmail.com>
2025-01-07 15:17:23 +0800

    Relayout SDGs section

--- frontend/src/routes/about-us/SDGsSection.jsx
+++ frontend/src/routes/about-us/SDGsSection.jsx
@@ -12,47 +12,61 @@ export default function SDGsSection() {
 
   return (
     <Container fluid style={{ background: 'red' }}>
-      <Container
-        css={css`
-          color: white;
-          text-align: center;
-          @media (min-width: 768px) {
-            width: 540px;
-          }
-        `}
-      >
+      <Container>
         <br />
-        <h2>
-          <b>We advance the Sustainable Development Goals.</b>
-        </h2>
-        <p style={{ fontSize: '20px' }}>
-          We believe in the Sustainable Development Goals (SDGs) and are
-          especially aiding the completion of SDG 3 (Good Health), 4 (Quality
-          Education), 5 (Gender Equality), and 13 (Climate Action).
-        </p>
+        <Container
+          css={css`
+            color: white;
+            text-align: center;
+            @media (min-width: 992px) {
+              width: 720px;
+            }
+          `}
+        >
+          <h2>
+            <b>We advance the Sustainable Development Goals.</b>
+          </h2>
+          <p style={{ fontSize: '20px' }}>
+            We believe in the Sustainable Development Goals (SDGs) and are
+            especially aiding the completion of SDG 3 (Good Health), 4 (Quality
+            Education), 5 (Gender Equality), and 13 (Climate Action).
+          </p>
+          <br />
+          <h3>SDGS WE SUPPORT:</h3>
+        </Container>
         <br />
-        <h3>SDGS WE SUPPORT:</h3>
-        <br />
-        <Row xs={2} className='g-4'>
-          {sdgs.map((sdg, i) => (
-            <Col key={i + 1}>
-              <Image
-                src={sdg}
-                alt={`SDG-${i + 1}`}
-                fluid
-                css={css`
-                  width: 100%;
-                  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-                  border-radius: 10px;
-                  object-fit: cover;
-                  border: 2px solid white;
+        <center>
+          <Row
+            xs={2}
+            sm={2}
+            md={4}
+            className='g-4'
+            css={css`
+              @media (min-width: 1200px) {
+                width: 960px;
+              }
+            `}
+          >
+            {sdgs.map((sdg, i) => (
+              <Col key={i + 1}>
+                <Image
+                  src={sdg}
+                  alt={`SDG-${i + 1}`}
+                  fluid
+                  css={css`
+                    width: 100%;
+                    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+                    border-radius: 10px;
+                    object-fit: cover;
+                    border: 2px solid white;
 
-                  ${OnHoverAnimationCss(1.07)}
-                `}
-              />
-            </Col>
-          ))}
-        </Row>
+                    ${OnHoverAnimationCss(1.07)}
+                  `}
+                />
+              </Col>
+            ))}
+          </Row>
+        </center>
         <br />
         <br />
       </Container>
