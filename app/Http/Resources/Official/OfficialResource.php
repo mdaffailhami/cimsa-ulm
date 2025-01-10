e74554e728741746705384c166085c34013c08d3
Dzakiy Dzakwan <dzakcart@gmail.com>
2025-01-11 00:12:34 +0700

    feat: add officials api endpoint

---
+++ app/Http/Resources/Official/OfficialResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\Official;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OfficialResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            "poster" => $this->poster,
+            "year" => $this->start_year,
+            "divisions" => OfficialDivisionResource::collection($this->divisions)
+        ];
+    }
+}
