2c17c3725ee0f4cf0711a6cb834c19f96d17bbec
Daffa Ilhami <mdaffailhami@gmail.com>
2025-01-14 11:34:11 +0800

    Setup backend to serve react

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// If route is_a not found (404), then return react-index.blade.php
+Route::fallback(function () {
+    return view('react-index');
 });
+
