32ad266d8b9fd5b7c00f2f7a73b624c5af96f10b
Daffa Ilhami <mdaffailhami@gmail.com>
2025-02-02 12:24:38 +0800

    Re-integrate Home Page API after pull

--- frontend/src/app/home/HomePage.jsx
+++ frontend/src/app/home/HomePage.jsx
@@ -71,7 +71,7 @@ export default function HomePage() {
       <hr />
       <NumberOfThingsSection
         statistics={JSON.parse(
-          page.data.contents.find((x) => x.column === 'statistics')
+          page.data.contents.find((x) => x.column === 'statistic')
             .long_text_content
         )}
       />
