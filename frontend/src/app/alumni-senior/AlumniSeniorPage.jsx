f2bc4d5aaae790766ad55cbb8b8964489cbee0e6
Daffa Ilhami <mdaffailhami@gmail.com>
2025-02-05 09:54:52 +0800

    Improve The Officials, Alumni & Senior, & Contact Us pages SEO

--- frontend/src/app/alumni-senior/AlumniSeniorPage.jsx
+++ frontend/src/app/alumni-senior/AlumniSeniorPage.jsx
@@ -5,16 +5,12 @@ import ContactCardSection from '../../components/ContactCardSection';
 import AlumniDistributionSection from './AlumniDistributionSection';
 import LoadingPage from '../../components/LoadingPage';
 import { endpoint } from '../../configs';
-import { fetchJSON, setPageMeta } from '../../utils';
+import { fetchJSON } from '../../utils';
 import HtmlParser from '../../components/HtmlParser';
 import useSWR from 'swr';
+import { PageMeta } from '../../components/PageMeta';
 
 export default function AlumniSeniorPage() {
-  setPageMeta(
-    'Alumni & Senior - CIMSA ULM',
-    'CIMSA ULM is forever thankful to those who have contributed their hearts, spirits, and time to making CIMSA ULM what it is today. This is a page dedicated to our alumni and seniors.'
-  );
-
   const page = useSWR(`${endpoint}/api/page/alumni-senior`, fetchJSON);
   const posts = useSWR(`${endpoint}/api/post?page=1&limit=3`, fetchJSON);
 
@@ -28,54 +24,60 @@ export default function AlumniSeniorPage() {
 
   return (
     <>
-      <PageHeader
-        title='Alumni & Senior'
-        description={
-          <HtmlParser
-            html={
-              page.data.contents.find((x) => x.column === 'description')
-                .long_text_content
-            }
-          />
-        }
+      <PageMeta
+        title='Alumni & Senior - CIMSA ULM'
+        description='CIMSA ULM is forever thankful to those who have contributed their hearts, spirits, and time to making CIMSA ULM what it is today.'
       />
-      <AlumniDistributionSection
-        image={
-          page.data.contents.find((x) => x.column === 'map-image').galleries[0]
-            .url
-        }
-      />
-      <br />
-      <br />
-      <BlogSection posts={posts.data.data} />
-      <br />
-      <br />
-      <ContactCardSection
-        header={
-          <div
-            data-aos='zoom-in'
-            data-aos-duration='1200'
-            data-aos-once='true'
-            css={css`
-              color: white;
-              text-align: center;
-              padding-top: 26px;
-              margin-bottom: -8px;
-            `}
-          >
-            <h3>Are You a CIMSA ULM Alumni or Senior?</h3>
-            <h4 style={{ fontWeight: 'normal' }}>
-              Contact us! We'd love to hear from you.
-            </h4>
-          </div>
-        }
-        period={page.data.contact.generation}
-        position={page.data.contact.occupation}
-        picture={page.data.contact.image}
-        name={page.data.contact.name}
-        email={page.data.contact.email}
-        phone={page.data.contact.phone}
-      />
+      <main>
+        <PageHeader
+          title='Alumni & Senior'
+          description={
+            <HtmlParser
+              html={
+                page.data.contents.find((x) => x.column === 'description')
+                  .long_text_content
+              }
+            />
+          }
+        />
+        <AlumniDistributionSection
+          image={
+            page.data.contents.find((x) => x.column === 'map-image')
+              .galleries[0].url
+          }
+        />
+        <br />
+        <br />
+        <BlogSection posts={posts.data.data} />
+        <br />
+        <br />
+        <ContactCardSection
+          header={
+            <div
+              data-aos='zoom-in'
+              data-aos-duration='1200'
+              data-aos-once='true'
+              css={css`
+                color: white;
+                text-align: center;
+                padding-top: 26px;
+                margin-bottom: -8px;
+              `}
+            >
+              <h3>Are You a CIMSA ULM Alumni or Senior?</h3>
+              <h4 style={{ fontWeight: 'normal' }}>
+                Contact us! We'd love to hear from you.
+              </h4>
+            </div>
+          }
+          period={page.data.contact.generation}
+          position={page.data.contact.occupation}
+          picture={page.data.contact.image}
+          name={page.data.contact.name}
+          email={page.data.contact.email}
+          phone={page.data.contact.phone}
+        />
+      </main>
     </>
   );
 }
--- frontend/src/app/officials/OfficialsPage.jsx
+++ frontend/src/app/officials/OfficialsPage.jsx
@@ -1,17 +1,13 @@
 import PageHeader from '../../components/PageHeader';
 import OrganizationStructure from './OrganizationStructure';
-import { fetchJSON, setPageMeta } from '../../utils';
+import { fetchJSON } from '../../utils';
 import { endpoint } from '../../configs';
 import LoadingPage from '../../components/LoadingPage';
 import HtmlParser from '../../components/HtmlParser';
 import useSWR from 'swr';
+import { PageMeta } from '../../components/PageMeta';
 
 export default function OfficialsPage() {
-  setPageMeta(
-    'The Officials - CIMSA ULM',
-    'Meet the officials of CIMSA ULM. We are a team of dedicated and passionate individuals who work together to achieve our goals and make a positive impact in our community.'
-  );
-
   const page = useSWR(`${endpoint}/api/page/officials`, fetchJSON);
   const officials = useSWR(`${endpoint}/api/official`, fetchJSON);
 
@@ -25,18 +21,24 @@ export default function OfficialsPage() {
 
   return (
     <>
-      <PageHeader
-        title='Meet the Officials'
-        description={
-          <HtmlParser
-            html={
-              page.data.contents.find((x) => x.column === 'description')
-                .long_text_content
-            }
-          />
-        }
+      <PageMeta
+        title='The Officials - CIMSA ULM'
+        description='Meet the officials of CIMSA ULM. We are a team of dedicated and passionate individuals who work together to achieve our goals and make a positive impact in our community.'
       />
-      <OrganizationStructure officials={officials.data.data} />
+      <main>
+        <PageHeader
+          title='Meet the Officials'
+          description={
+            <HtmlParser
+              html={
+                page.data.contents.find((x) => x.column === 'description')
+                  .long_text_content
+              }
+            />
+          }
+        />
+        <OrganizationStructure officials={officials.data.data} />
+      </main>
     </>
   );
 }
