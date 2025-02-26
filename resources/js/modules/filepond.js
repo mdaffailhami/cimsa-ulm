5d13284817b2704f6088e0984ecc7f16f0f30b84
Dzakiy Dzakwan <dzakcart@gmail.com>
2025-02-26 13:17:26 +0700

    feat: adding size and type validation on filepond

--- resources/js/modules/filepond.js
+++ resources/js/modules/filepond.js
@@ -1,16 +1,27 @@
 import * as FilePond from "filepond";
 import "filepond/dist/filepond.min.css";
+
 import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size';
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+
 import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
 
 FilePond.registerPlugin(FilePondPluginImagePreview);
+FilePond.registerPlugin(FilePondPluginFileValidateSize);
+FilePond.registerPlugin(FilePondPluginFileValidateType);
 
 const initializeImagePond = (input, allowMultiple = false, maxFiles = 1) => {
     return FilePond.create(input, {
         allowMultiple: allowMultiple,
         maxFiles: maxFiles,
         allowImagePreview: true,
-        acceptedFileTypes: ["image/*"],
+        acceptedFileTypes: ["image/png", "image/jpeg"],
+        labelFileTypeNotAllowed : "Format file tidak tepat",
+        fileValidateTypeLabelExpectedTypes : "Hanya dapat menerima file png, jpg, jpeg",
+        maxFileSize: "1MB",
+        labelMaxFileSizeExceeded : "Size file terlalu besar",
+        labelMaxFileSize : "Besar maksimal {filesize}",
         server: {
             headers: {
                 "X-CSRF-TOKEN": "{{ csrf_token() }}",
@@ -20,8 +31,13 @@ const initializeImagePond = (input, allowMultiple = false, maxFiles = 1) => {
                 method: "POST",
                 withCredentials: false,
                 onload: (response) => {
-                    response = JSON.parse(response);
-                    return response; // Return the file path to FilePond
+                    try {
+                        const parsedResponse = JSON.parse(response);
+                        return parsedResponse;
+                    } catch (error) {
+                        console.error("Invalid JSON response:", response);
+                        return null;
+                    }
                 },
                 onerror: (response) => {
                     console.error("Error uploading file:", response);
